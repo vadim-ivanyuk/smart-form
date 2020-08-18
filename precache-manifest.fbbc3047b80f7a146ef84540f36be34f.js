ac6c94c4e326de70b0b717d7d72b79a4efd0c00f
oceantraffic <vadim.ivanyuk.126@gmail.com>
2020-08-18 14:17:02 +0300

    Updates

---
+++ precache-manifest.fbbc3047b80f7a146ef84540f36be34f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4c4f30e416174c181fce226f3e34748b",
+    "url": "/smart-form/index.html"
+  },
+  {
+    "revision": "e6349729968ce6d7a8c2",
+    "url": "/smart-form/static/css/2.ed439b8d.chunk.css"
+  },
+  {
+    "revision": "1390abd1cb7808883d3b",
+    "url": "/smart-form/static/css/main.054401a1.chunk.css"
+  },
+  {
+    "revision": "e6349729968ce6d7a8c2",
+    "url": "/smart-form/static/js/2.76742763.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/smart-form/static/js/2.76742763.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1390abd1cb7808883d3b",
+    "url": "/smart-form/static/js/main.7e5cf069.chunk.js"
+  },
+  {
+    "revision": "9e6e1427756156eca89d",
+    "url": "/smart-form/static/js/runtime-main.d28cf6db.js"
+  }
+]);
